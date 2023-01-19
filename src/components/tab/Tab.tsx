f6567e0c7ad13e861b19897dd230199b681dd42b
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-19 12:53:30 -0300

    style: add grid to words tab

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -22,62 +22,36 @@ export default function Tab() {
             </span>
           ),
           key: '1',
-          children: [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-            20,
-          ].map((element) => {
+          children: [1].map((element) => {
             return (
               <div key={element}>
-                <Row
-                  justify='space-around'
-                  style={{ padding: '10px 0' }}
-                  className={styles['list-row']}
-                  //   onClick={() => setItemsSelected('2')}
-                >
-                  <Col span={0.1}>
-                    <Row align='middle' style={{ flexDirection: 'column' }}>
-                      <Text strong>Airplane</Text>
-                    </Row>
+                <Row gutter={[16, 24]}>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Airplane</div>
                   </Col>
-
-                  <Col>
-                    <Row align='middle' style={{ flexDirection: 'column' }}>
-                      <Text strong>Verb/Noun</Text>
-                    </Row>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Box</div>
                   </Col>
-
-                  {/* <Col>
-                    <Row align='middle' style={{ flexDirection: 'column' }}>
-                      <NotificationOutlined style={{ fontSize: '1rem' }} />
-                    </Row>
-                  </Col> */}
-
-                  {/* <Col>
-                    <Row
-                      align='middle'
-                      style={{ flexDirection: 'column', fontSize: '1rem' }}
-                    >
-                      <StarOutlined />
-                    </Row>
-                  </Col> */}
-                  {/* <Col span={16}>
-              <Row justify='space-between'>
-                <Title level={5} underline>
-                  {'element.usuario'}
-                </Title>
-                <Row justify='space-around'>
-                  <Row align='middle' style={{ flexDirection: 'column' }}>
-                    <Text strong>
-                      longDateThreeLetterMonth
-                    </Text>
-                    <Text strong>
-                      hourMinuteSecond
-                    </Text>
-                  </Row>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Word</div>
+                  </Col>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Programming</div>
+                  </Col>
                 </Row>
-              </Row>
-              <Row>{'<Progress percent={100} />'}</Row>
-            </Col> */}
+                <Row gutter={[16, 24]}>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Airplane</div>
+                  </Col>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Box</div>
+                  </Col>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Word</div>
+                  </Col>
+                  <Col className='gutter-row' span={6}>
+                    <div className={styles['gutter-box']}>Programming</div>
+                  </Col>
                 </Row>
               </div>
             )
