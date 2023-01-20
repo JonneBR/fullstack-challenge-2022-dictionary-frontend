cd3dfcf866f9e5e9131615c4a89d9cb567c27ea0
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-20 15:40:15 -0300

    style: add favorite icon to container-dictionary

--- src/components/Dictionary/Dictionary.tsx
+++ src/components/Dictionary/Dictionary.tsx
@@ -1,8 +1,12 @@
+import { StarTwoTone } from '@ant-design/icons'
 import styles from './styles.module.scss'
 
 export default function Dictionary() {
   return (
     <div className={styles['container-dictionary']}>
+      <div className={styles['favorite-icon']}>
+        <StarTwoTone />
+      </div>
       <h2>hello</h2>
       <h3>həˈləʊ</h3>
     </div>
