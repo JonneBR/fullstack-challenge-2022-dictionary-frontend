e8e21737ec48c08d504e9da25b7d63d2b44cdfb4
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-23 13:30:04 -0300

    refactor: create AxiosHttpGet production class

--- src/data/protocols/http-get-client.ts
+++ src/data/protocols/http-get-client.ts
@@ -1,8 +1,19 @@
+export type HttpRequest = {
+  url: string
+  method: 'get'
+  body?: any
+  headers?: any
+}
 export interface HttpResponse<T = unknown> {
-  statusCode: 200
+  statusCode: HttpStatusCode
   body?: T
 }
 
 export interface HttpGetClient<R = unknown> {
   get(url: string): Promise<HttpResponse<R>>
+}
+
+export enum HttpStatusCode {
+  ok = 200,
+  notFound = 404,
 }
---
+++ src/infra/http/axios-http-get.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosResponse } from 'axios'
+import { HttpGetClient, HttpResponse } from '../../data/protocols'
+
+export class AxiosHttpGet implements HttpGetClient {
+  async get(url: string): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse
+    try {
+      axiosResponse = await axios.request({
+        url,
+      })
+    } catch (error: any) {
+      axiosResponse = error.response
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    }
+  }
+}
